df528948cf45e51df65724781d74097ef85ab778
Krunali <krunalilole29@gmail.com>
2022-04-08 22:17:45 +0530

    Adding UC 3 to Remove avoidable words in Paragraph

--- src/com/bridgelabz/HashTables/MyLinkedHashMapTest.java
+++ src/com/bridgelabz/HashTables/MyLinkedHashMapTest.java
@@ -21,4 +21,24 @@ public class MyLinkedHashMapTest {
         int frequency = myLinkedHashMap.get("paranoid");
         Assert.assertEquals(3,frequency);
     }
+    @Test
+    public void givenASentence_WhenAWordIsGiven_ShouldDeleteTheWord() {
+        String sentence = "Paranoid are not paranoid because they are paranoid " +
+                "because they keep putting themselves deliberately into" +
+                "paranoid avoidable situation";
+        MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+        String[] words = sentence.split(" ");
+        for (String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null) {
+                value = 1;
+            } else {
+                value += 1;
+            }
+            myLinkedHashMap.add(word, value);
+        }
+        boolean result = myLinkedHashMap.delete("avoidable");
+        System.out.println("My linked list after deleting is : " + myLinkedHashMap);
+        Assert.assertTrue(result);
+    }
 }
